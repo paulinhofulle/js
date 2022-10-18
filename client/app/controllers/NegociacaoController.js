b89ee25b597b5dbc7f81a462cacec27a55cc9d1a
paulinho.fulle <paulohfulle@gmail.com>
2022-10-17 21:16:15 -0300

    video 11

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -1,38 +1,53 @@
 class NegociacaoController{
 
-constructor(){
-    let $ = document.querySelector.bind(document); 
+    constructor(){
+        let $ = document.querySelector.bind(document); 
 
-    this._inputData = $("#data");
-    this._inputQuantidade = $("#quantidade");
-    this._inputValor = $("#valor");
-}    
+        this._inputData = $("#data");
+        this._inputQuantidade = $("#quantidade");
+        this._inputValor = $("#valor");
+        this._negociacoes = new negociacoes();
+    }    
 
-adiciona(evento){
-    event.preventDefault();
+    adiciona(evento){
+        event.preventDefault();
 
-    let converter = new DateConverter();
+        let converter = new DateConverter();
 
-    console.log(typeof this._inputData.value.split('-'));
-    let data = DateConverter.paraData(this._inputData.value);
-    console.log(data);
-    let negociacao = new negociacao(data, parseInt(this._inputQuantidade), parseInt(this._inputValor));
+        console.log(typeof this._inputData.value.split('-'));
+        console.log(data);
 
-    let diaMesAno = DateConverter.paraTexto(data);
+        let diaMesAno = DateConverter.paraTexto(data);
 
-    console.log(diaMesAno);
+        console.log(diaMesAno);
 
 
-    console.log('controller funcionando');
+        console.log('controller funcionando');
 
-    
+        this._negociacoes.adiciona(this._criaNegociacao);
 
-    console.log(this._inputData.value);
-    console.log(parseInt(this._inputQuantidade.value));
-    console.log(parseFloat(this._inputValor.value));
+        console.log(this._negociacoes.paraArray());
 
-    console.log(typeof (inputQuantidade.value));
+        console.log(this._inputData.value);
+        console.log(parseInt(this._inputQuantidade.value));
+        console.log(parseFloat(this._inputValor.value));
 
-}
+        console.log(typeof (inputQuantidade.value));
+
+        this._limparFormulario;
+
+    }
+
+    _limparFormulario(){
+        this._inputData.value = '2020-01-01';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 3;
+        this._inputData.focus();
+
+    }
+
+    _criaNegociacao(){
+        return new negociacao(DateConverter.paraData(this._inputData.value), parseInt(this._inputQuantidade), parseInt(this._inputValor));
+    }
 
 }
---
+++ client/app/domain/negociacao/negociacoes.js
@@ -0,0 +1,14 @@
+class negociacoes{
+
+    constructor(){
+        this._negociacoes = [];
+    }
+
+    adiciona(negociacao){
+        this._negociacoes.push(negociacao);
+    }
+
+    paraArray(){
+        return [].concat(this._negociacoes);
+    }
+}
